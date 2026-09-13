1e4e88020e6b2153bb612aca5bbed9fa5f18419d
showads <casedeposit@gmail.com>
2026-09-12 22:24:30 -0500

    Avoid auto-loading standalone app script in main ComfyUI UI

--- __init__.py
+++ __init__.py
@@ -2,8 +2,7 @@ from .server import register_routes
 
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
-WEB_DIRECTORY = "./web"
 
 register_routes()
 
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
